2121497d54de82985cd984b3c6065c1acda52b13
helgaathifah05@gmail.com <helgaathifah05@gmail.com>
2026-03-10 09:07:58 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 158, 101, 140)),
-        useMaterial3: true, // Tambahkan ini agar tampilan lebih modern (Material 3)
+        useMaterial3: true,
       ),
       home: const OnboardingView(),
     );
